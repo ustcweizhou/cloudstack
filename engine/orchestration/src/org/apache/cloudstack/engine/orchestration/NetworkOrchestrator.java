ustcweizhou/cloudstack

commit 8218ce0fe243d85d0f4cb6280d2310a81c8e99de

unexplained happy flow for restart network modified

engine/orchestration/src/org/apache/cloudstack/engine/orchestration/NetworkOrchestrator.java
@@ -2464,12 +2464,11 @@ public class NetworkOrchestrator extends ManagerBase implements NetworkOrchestra
         try {
             implementNetworkElementsAndResources(dest, context, network, offering);
             setRestartRequired(network, true);
+            return true;
         } catch (Exception ex) {
             s_logger.warn("Failed to implement network " + network + " elements and resources as a part of network restart due to ", ex);
             return false;
         }
-        setRestartRequired(network, false);
-        return true;
     }
 
     private void setRestartRequired(NetworkVO network, boolean restartRequired) {

commit b189a7b778976b100964210644fbcb61a75e66c4

CLSTACK-883: fix issue restartRequired of network is not changed after restartNetwork
This issue was introduced in commit 8218ce0fe243d85d0f4cb6280d2310a81c8e99de

engine/orchestration/src/org/apache/cloudstack/engine/orchestration/NetworkOrchestrator.java
@@ -2556,7 +2556,7 @@ public class NetworkOrchestrator extends ManagerBase implements NetworkOrchestra
 
         try {
             implementNetworkElementsAndResources(dest, context, network, offering);
-            setRestartRequired(network, true);
+            setRestartRequired(network, false);
             return true;
         } catch (final Exception ex) {
             s_logger.warn("Failed to implement network " + network + " elements and resources as a part of network restart due to ", ex);