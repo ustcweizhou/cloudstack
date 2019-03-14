ustcweizhou/cloudstack

commit 9c7cd8c2485412bc847b2c2473b962fa01435b24

CLOUDSTACK-10081: CloudUtils getDevInfo function will now return "bridge" instead of "dev" when the name of a ovs bridge is passed.

python/lib/cloudutils/networkConfig.py
@@ -154,7 +154,7 @@ class networkConfig:
 
         if networkConfig.isBridgePort(dev):
             type = "brport"
-        elif networkConfig.isBridge(dev):
+        elif networkConfig.isBridge(dev) or networkConfig.isOvsBridge(dev):
             type = "bridge"
         else:
             type = "dev"

commit 72470efba3e43a7b24763382678d3999c92aa903

ubuntu16: fix unable to add host if cloudbrX is not configured
while add a ubuntu16.04 host with native eth0 (cloudbrX is not configured),
the operation failed and I got the following error in /var/log/cloudstack/agent/setup.log

```
DEBUG:root:execute:ifconfig eth0
DEBUG:root:[Errno 2] No such file or directory
  File "/usr/lib/python2.7/dist-packages/cloudutils/serviceConfig.py", line 38, in configration
    result = self.config()
  File "/usr/lib/python2.7/dist-packages/cloudutils/serviceConfig.py", line 211, in config
    super(networkConfigUbuntu, self).cfgNetwork()
  File "/usr/lib/python2.7/dist-packages/cloudutils/serviceConfig.py", line 108, in cfgNetwork
    device = self.netcfg.getDefaultNetwork()
  File "/usr/lib/python2.7/dist-packages/cloudutils/networkConfig.py", line 53, in getDefaultNetwork
    pdi = networkConfig.getDevInfo(dev)
  File "/usr/lib/python2.7/dist-packages/cloudutils/networkConfig.py", line 157, in getDevInfo
    elif networkConfig.isBridge(dev) or networkConfig.isOvsBridge(dev):
```

The issue is caused by commit 9c7cd8c2485412bc847b2c2473b962fa01435b24
2017-09-19 16:45 Sigert Goeminne ● CLOUDSTACK-10081: CloudUtils getDevInfo function will now return "bridge" instead o

python/lib/cloudutils/networkConfig.py
@@ -103,6 +103,9 @@ class networkConfig:
 
     @staticmethod
     def isOvsBridge(devName):
+        cmd = bash("which ovs-vsctl")
+        if not cmd.isSuccess():
+            return False
         try:
             return 0==subprocess.check_call(("ovs-vsctl", "br-exists", devName))
         except subprocess.CalledProcessError: