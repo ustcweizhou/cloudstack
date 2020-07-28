53788c772b12191fd5136bde23c749636d8f4973
Wei Zhou <w.zhou@global.leaseweb.com>
2020-07-28 06:34:49 +0000

    Ubuntu 20.04/MySQL 8: fix some issues in db

--- engine/schema/src/main/resources/META-INF/db/schema-41310to41400.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41310to41400.sql
@@ -19,6 +19,9 @@
 -- Schema upgrade from 4.13.1.0 to 4.14.0.0
 --;
 
+-- mysql8 nics table fix for newer distributions
+ALTER TABLE `cloud`.`nics` MODIFY COLUMN update_time timestamp DEFAULT CURRENT_TIMESTAMP;
+
 -- Update the description to indicate this only works with KVM + Ceph 
 -- (not implemented properly atm for KVM+NFS/local, and it accidentaly works with XS + NFS. Not applicable for VMware)
 UPDATE `cloud`.`configuration` SET `description`='Indicates whether to always backup primary storage snapshot to secondary storage. Keeping snapshots only on Primary storage is applicable for KVM + Ceph only.' WHERE  `name`='snapshot.backup.to.secondary';
